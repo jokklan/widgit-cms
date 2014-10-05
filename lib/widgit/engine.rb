7536157779b0dc25eeb9b083eaa8641442b39304
Johan Frølich <johanfrolich@gmail.com>
2014-10-05 22:44:10 +0200

    Renamed admin assets

--- lib/widgit/engine.rb
+++ lib/widgit/engine.rb
@@ -1,5 +1,16 @@
 module Widgit
   class Engine < ::Rails::Engine
     isolate_namespace Widgit
+
+    # Add admin assets to be precompiled
+    initializer :assets do |config|
+      Rails.application.config.assets.precompile += %w(widgit/admin.js)
+      Rails.application.config.assets.precompile += %w(widgit/admin.css)
+    end
+
+    # Add app/widgets to view path, for apotomo
+    initializer :apotomo do |config|
+      Apotomo::Widget.append_view_path root + 'app/widgets'
+    end
   end
 end
