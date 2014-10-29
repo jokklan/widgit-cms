f3f2e19128f7fde5091a2d0efc9b74c15461b540
Johan Frølich <johanfrolich@gmail.com>
2014-10-29 23:54:22 +0100

    Structured javascript files

--- app/assets/javascripts/widgit/admin.js
+++ app/assets/javascripts/widgit/admin.js
@@ -1,25 +1,2 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-// jQuery
-//= require jquery
-
-// Twitter Bootstrap
-//= require bootstrap
-//= require bootstrap-sprockets
-
-// Rails
-//= require jquery_ujs
-//= require turbolinks
-
-// Modules
-//= require widgit/modules/tooltips
+//= require_tree ./admin/modules
+//= require_tree ./admin/plugins
---
+++ app/assets/javascripts/widgit/vendors.js
@@ -0,0 +1,14 @@
+// jQuery
+//= require jquery
+//= require jquery-ui
+
+// Twitter Bootstrap
+// require bootstrap
+// require bootstrap-sprockets
+
+// Rails
+//= require jquery_ujs
+//= require turbolinks
+
+//= require rangy/rangy-core
+//= require hallo
