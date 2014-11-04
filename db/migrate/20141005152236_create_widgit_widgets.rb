f204991ad28d003d90cb728886c7a440e4706994
Johan Frølich <johanfrolich@gmail.com>
2014-11-04 12:54:51 +0100

    Added columns to widgits

--- db/migrate/20141005152236_create_widgit_widgets.rb
+++ db/migrate/20141005152236_create_widgit_widgets.rb
@@ -3,6 +3,7 @@ class CreateWidgitWidgets < ActiveRecord::Migration
     create_table :widgit_widgets do |t|
       t.references :widget_group, index: true
       t.string :type, null: false
+      t.integer :columns, null: false
       t.hstore :properties
 
       t.timestamps null: false
