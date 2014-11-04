d5e683051bb3f5a1309d8b671f6caf20d28bffb1
Johan Frølich <johanfrolich@gmail.com>
2014-11-04 13:01:56 +0100

    Made position a required field on widget groups

--- app/models/widgit/widget_group.rb
+++ app/models/widgit/widget_group.rb
@@ -6,5 +6,23 @@ module Widgit
     # Associations
     belongs_to :widgetable, polymorphic: true
     has_many :widgets
+
+    # Validations
+    validates :position, presence: true
+
+    # Callbacks
+    before_validation :set_position, on: :create
+
+  private
+
+    def set_position
+      self.position ||= begin
+        if widgetable && (last_widget_group = widgetable.widget_groups.last)
+          last_widget_group.position + 1
+        else
+          0
+        end
+      end
+    end
   end
 end
--- db/migrate/20141030110349_add_position_to_widgit_widget_groups.rb
+++
@@ -1,5 +0,0 @@
-class AddPositionToWidgitWidgetGroups < ActiveRecord::Migration
-  def change
-    add_column :widgit_widget_groups, :position, :integer
-  end
-end
