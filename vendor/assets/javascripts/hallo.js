5c168e7d512336772fab13ceb23fd15e6ab56e0b
joachimbl <joachim.blicher@gmail.com>
2014-11-16 21:44:38 +0100

    reorder styling

--- vendor/assets/javascripts/hallo.js
+++ vendor/assets/javascripts/hallo.js
@@ -1050,16 +1050,16 @@
               throw new Error "SelectionNotSet"
             catch error
               widget.options.editable.restoreSelection(widget.lastSelection)
-      
+
             document.execCommand "insertImage", null, jQuery(this).attr('src')
             img = document.getSelection().anchorNode.firstChild
             jQuery(img).attr "alt", jQuery(".caption").value
-      
+
             triggerModified = () ->
               widget.element.trigger "hallomodified"
             window.setTimeout triggerModified, 100
             widget._closeDialog()
-      
+
             addImage = "##{widget.options.uuid}-#{widget.widgetName-addimage"
             @options.dialog.find(".halloimage-activeImage, addImage).click insertImage
       */
@@ -2554,7 +2554,7 @@
         toolbarCssClass: null,
         toolbarPositionAbove: false,
         toolbarOptions: {},
-        placeholder: '',
+        placeholder: 'Lorem Ipsum',
         forceStructured: true,
         checkTouch: true,
         touchScreen: null
