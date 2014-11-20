2df0b3dc4fafcc808b4a8c25ea70db14c288e4b1
joachimbl <joachim.blicher@gmail.com>
2014-11-20 19:57:31 +0100

    asset path

---
+++ app/models/widgit/widget.rb
@@ -0,0 +1,43 @@
+module Widgit
+  class Widget < ActiveRecord::Base
+    # Configuration
+    default_scope { order(:position) }
+
+    # Association
+    belongs_to :widget_group, inverse_of: :widgets
+    has_one :page, through: :widget_group
+
+    # Validations
+    validates :widget_group, :type, :columns, :position, presence: true
+
+    # Callbacks
+    before_validation :set_position, on: :create
+
+    # Instance Methods
+    def to_json
+      attributes.symbolize_keys.compact.slice(:id, :type, :text, :position, :columns).to_json
+    end
+
+    def widget_type
+      type.demodulize.downcase
+    end
+
+    # Class Methods
+    def self.types
+      %w(header text image)
+    end
+
+  private
+
+    # Private Instance Methods
+    def set_position
+      self.position ||= begin
+        if widget_group && (last_widget = widget_group.widgets.last)
+          last_widget.position.to_i + 1
+        else
+          0
+        end
+      end
+    end
+  end
+end
