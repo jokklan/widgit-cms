a51f8a7b42b4fe47684a285f6ec289b997de4680
Johan Frølich <johanfrolich@gmail.com>
2015-01-29 22:58:48 +0100

    Refactored configuration panel

--- app/helpers/widgit/base_helper.rb
+++ app/helpers/widgit/base_helper.rb
@@ -3,5 +3,15 @@ module Widgit
     def target_id(record)
       record.id || (Time.now.to_f * 1000).round
     end
+
+    def resource_classes
+      Widgit.components.map { |component| "Widgit::Components::#{component.classify}".constantize }
+        .push(Widgit::Block)
+    end
+
+    def widgit_form_for(object, *args, &block)
+      options = args.extract_options!
+      simple_form_for(object, *(args << options.merge(builder: Widgit::FormBuilder)), &block)
+    end
   end
 end
