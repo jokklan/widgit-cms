d931200a8594ff76c78a6e88dc7c9518f3ecb54a
Johan Frølich <johanfrolich@gmail.com>
2015-02-01 12:14:53 +0100

    Refactored image editor

--- app/inputs/widgit/image_input.rb
+++ app/inputs/widgit/image_input.rb
@@ -1,7 +1,11 @@
 module Widgit
-  class ImageInput < SimpleForm::Inputs::FileInput
+  class ImageInput < SimpleForm::Inputs::StringInput
     def input
       template.content_tag :div, super, data: { editor: 'image' }
+    end
+
+    def input_type
+      :string
     end
   end
 end
