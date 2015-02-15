32b53ac701b7aa3d1d5c590f02a2fe3a23b1c2b1
theisof <theisof@gmail.com>
2015-02-15 19:53:47 +0100

    font size on icons

--- app/models/widgit/components/icon.rb
+++ app/models/widgit/components/icon.rb
@@ -4,12 +4,18 @@ module Widgit
       # Extensions
       hstore_accessor :properties, { 
                         icon: :string,
+                        font_size: :integer, 
                         text_align: :integer 
                       }
       configurable icon: { 
                     data_type: :icon, 
                     input_type: :string 
                   },
+                  font_size: { 
+                    data_type: :font_size, 
+                    input_type: :slider, 
+                    default_value: 2 
+                  },                    
                   text_align: { 
                     data_type: :text_align, 
                     input_type: :slider, 
